dd9e47dc20d70f0f60f0ef226be458fe82704ff6
bortch89 <bortch@gmail.com>
2017-05-21 00:55:46 +0200

    Fixed omniauth. Also set email validation to have 30 days to be validated

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,15 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
-         :confirmable, :omniauthable          
+         :confirmable, :omniauthable, :omniauth_providers => [:facebook]
 
   validates :username, presence: true, length: {maximum: 30}
+
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.username = auth.info.name
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0,20]
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
 
   root "static#home"
-  
+
+  get '/auth/facebook/callback' => 'sessions#create'
+
 end
