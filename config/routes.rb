bf02a1969472833a76cbff846b8bce6862eb7fb2
bortch89 <bortch@gmail.com>
2017-05-21 22:13:24 +0200

    added description textarea to edit form, and setup devise controller to overwrite the default so that users who signed up with facebook can edit their profiles without a password

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users,
-             :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" },
+             :controllers => { :omniauth_callbacks => "users/omniauth_callbacks",
+                               :registrations => 'registrations'  
+                              },
              :path => '',
              :path_names => {:sign_in => 'login', :sign_out => 'logout', :edit => 'profile'}
 
