f67fc4c53628b634c8a48876067af249aab89174
bortch89 <bortch@gmail.com>
2017-05-22 11:17:05 +0200

    Created rooms model controller and migration - went a little crazy on the fields

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170521171406) do
+ActiveRecord::Schema.define(version: 20170522091552) do
+
+  create_table "rooms", force: :cascade do |t|
+    t.string "home_type"
+    t.string "room_type"
+    t.integer "capacity"
+    t.integer "bed_rooms"
+    t.integer "bath_room"
+    t.string "listing_name"
+    t.text "summary"
+    t.string "address"
+    t.integer "price"
+    t.boolean "active"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
